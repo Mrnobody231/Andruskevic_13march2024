c04420602fde153cb3fa3d367d5bae58ac78ce6a
Mrnobody231 <andruskevic.e@gmail.com>
2024-09-29 19:05:11 +0300

    Added Title in the sql

--- SQL.sql
+++ SQL.sql
@@ -1,23 +1,29 @@
+1. Выведите все уникальные названия продуктов
 SELECT DISTINCT product_name
 FROM Products
 
+2. Выведите id, название и стоимость продуктов с содержанием клетчатки (fiber) более 5 граммов.
 SELECT p.product_id, p.product_name, p.price
 FROM Products AS p
 JOIN Nutritional_Information AS n ON p.product_id = n.product_id
 WHERE n.fiber > 5
 
+3. Выведите название продукта с самым высоким содержанием белка (protein).
 SELECT p.product_name
 FROM Products AS p
 JOIN Nutritional_Information AS n ON p.product_id = n.product_id
 ORDER BY n.protein DESC
 LIMIT 1
 
+4. Подсчитайте общую сумму калорий для продуктов каждой категории, но не учитывайте продукты с нулевым жиром (fat = 0).
+Выведите id категории, сумму калорий.
 SELECT p.category_id, SUM(p.calories) 
 FROM Products AS p
 JOIN Nutritional_Information AS n ON p.product_id = n.product_id
 WHERE n.fat > 0
 GROUP BY p.category_id
 
+5. Рассчитайте среднюю цену товаров каждой категории. Выведите название категории, среднюю цену.
 SELECT c.category_name, AVG(p.price) 
 FROM Products AS p
 JOIN Categories AS c ON p.category_id = c.category_id
